da15d53ea4095b1fbff71a89436c2cf656723c6e
Daniel Krippner <daniel.krippner@etas.com>
2026-05-13 15:12:01 +0200

    Connect mqtt5 transport at startup

--- up-subscription-cli/src/transport/mqtt5.rs
+++ up-subscription-cli/src/transport/mqtt5.rs
@@ -11,10 +11,11 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
+use backon::{ExponentialBuilder, Retryable};
 use tracing::info;
-use up_rust::UTransport;
+use up_rust::{UCode, UTransport};
 use up_transport_mqtt5::{Mqtt5Transport, Mqtt5TransportOptions};
 
 pub(crate) async fn get_mqtt5_transport(
@@ -22,7 +23,24 @@ pub(crate) async fn get_mqtt5_transport(
     mqtt5_args: Mqtt5TransportOptions,
 ) -> Result<Arc<dyn UTransport>, Box<dyn std::error::Error>> {
     info!("Using MQTT 5 uProtocol transport");
-    Ok(Mqtt5Transport::new(mqtt5_args, authority_name)
+
+    let transport = Mqtt5Transport::new(mqtt5_args, authority_name)
         .await
-        .map(Arc::new)?)
+        .map(Arc::new)?;
+
+    (|| transport.connect())
+                    .retry(
+                        ExponentialBuilder::default().with_total_delay(Some(Duration::from_secs(10))),
+                    )
+                    .notify(|error, sleep_duration| {
+                        info!("Attempt to connect to MQTT broker failed [error: {error}], retrying in {sleep_duration:?}");
+                    })
+                    .when(|err| {
+                        // no need to keep retrying if authentication or permission is denied
+                        err.get_code() != UCode::UNAUTHENTICATED
+                            && err.get_code() != UCode::PERMISSION_DENIED
+                    })
+                    .await?;
+    info!("Connected to MQTT5 broker");
+    Ok(transport)
 }
